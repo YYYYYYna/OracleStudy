6b573ba2a6608d709b2838868696810d34845c59
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 <officeight@naver.com>
2023-11-03 14:15:21 +0900

    11.03 14:15

--- FirstJDBCProject/src/com/sist/main/MainClass.java
+++ FirstJDBCProject/src/com/sist/main/MainClass.java
@@ -1,11 +1,47 @@
 package com.sist.main;
+import java.sql.*;
 
 public class MainClass {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Connection conn = null;
+		try {
+			//핵심 => 자바 ( 12장 )
+			Class.forName("oracle.jdbc.driver.OracleDriver");//오라클 연결 => 드라이버 설정
+			System.out.println("드라이버로딩성공");
+			// 오라클 연결 connect hr/happy
+			String url = "jdbc:oracle:thin:@localhost:1521:XE";
+			// 네트워크 => ip/port XE=> 데이터베이스 이름
+			// 파일 / 폴더
+			// 테이블 / 데이터베이스 => book,emp,dept(테이블)
+			String id = "hr";
+			String pwd = "happy";
+			
+			/////////////내가임의추가한내용//////////////Connection con = DriverManager.getConnection(url, user, password);
+	        /////////////내가임의추가한내용//////////////System.out.println("연결성공:"+con);
+			
+			conn = DriverManager.getConnection(url,id,pwd);// 연결
+			String sql = "SELECT ename,hiredate,job FROM emp";
+			ps = conn.prepareStatement(sql);//쿼리문 송신
+			rs = ps.executeQuery();//쿼리문 수신
+			while(rs.next()) {
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		finally {
+			try {
+				rs.close();
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
-
 	}
-
 }
--- FirstJDBCProject/src/com/sist/main/MainClass_2.java
+++ FirstJDBCProject/src/com/sist/main/MainClass_2.java
@@ -22,8 +22,58 @@ package com.sist.main;
  *     => 테스트
  *     => 배포(발표) ==> 호스팅(AWS)
  *     
+ *     클래스 : 관련된 데이터를 묶는다 => VO
+ *             => 캡슐화 => 변수의은닉화(private)
+ *               => 다른클래스나 브라우저에서 접근이 안됨
+ *               => 사용 => 읽기/쓰기 => getter/setter
+ *            => 사용자 정의 데이터
+ *               => CRUD => 액션
+ *                  => 메소드로만 만든다
+ *                  => ~Manager
+ *                  => ~DAO
+ *            관련된 기능을 묶는다
+ *            => 핵심=재사용(상속/**포함)
+ *            => 관련된 클래스가 여러개 있는 경우***=>인터페이스의 정의로 대답해야함!!!
+ *              => 인터페이스를 이용해서 1개로 제어
+ *                 List list=new ArrayList()
+ *                      list=new Vector()
+ *                      list=new LinkedList()
+ *              => 객체란?
+ *                 Object:현실이나 비현실 세계를 단순화하는것
+ *              *** 여러개의 데이터
+ *                  변수/배열/클래스 => 한개의 이름으로 제어
+ *     
+ *     예)
+ *     emp => 사원정보
+ *     =>empno : 사번 => int
+ *     =>ename : 이름 => String
+ *     =>job   : 직위 => String
+ *     =>hiredate : 입사일 => Date
+ *     =>sal   : 급여 => int
+ *     =>comm : 성과금 => int
+ *     =>mgr : 사수의 사번 => int
+ *     =>deptno : 부서번호 => int
+ *     
+ *     리턴형이 1개 밖에 없기 때문에
+ *     <기존의 하던 방식> 처럼 이렇게 하나씩 다 만들거나...
+ *     => public int getEmopno()
+ *        public String getEname()
+ *        public String getJob()
+ *        public Date getHiredate()
+ *        public int getSal()
+ *        public int getComm()
+ *        public int getMgr()
+ *        public int getDeptno()
+ *        : 이렇게 데이터를 한 8개를 만들어서 보내야함....
+ *        : 데이터가 많을수록 속도가 느려지고 불편해짐
+ *     
+ *     리턴형이 1개 밖에 없기 때문에
+ *     <VO를 통해 하나로 묶어서 효율적으로 사용가능>
+ *     => public EmpVo getEmpData()
+ *     
  *          
- *          react는 데이터 관리가 근간임
+ *          **react는 데이터 관리가 근간임
+ *          **비행기 게임에서 비행기는 "움직이는"게 아니라 기존위치를 삭제하고 해당 좌표점에 다시 그려주는거임
  *          
  *        
  */
